187e40ec57b30db6c4eea8743d43f244e6ebeab5
Depriest43 <depriest43@gmail.com>
2023-12-02 00:24:29 -0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CisGaAssignment2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void btnDisplayShortMessage_Clicked(object sender, EventArgs e)
+        {
+            string displayMessage = entDisplayMessage.Text;
+            editDisplay.Text = displayMessage;
+        }
+        private void btnAppendShortMessage_Clicked(object sender, EventArgs e)
+        {
+            string displayMessage = entDisplayMessage.Text;
+            editDisplay.Text = displayMessage += displayMessage;
+        }
+
+        private void btnClear_Clicked(object sender, EventArgs e)
+        {
+
+
+            string longMessageToAppend = editUserLongMessage.Text;
+            editDisplay.Text += longMessageToAppend;
+            {
+                entDisplayMessage.Text = "";
+                editDisplay.Text = "";
+                editUserLongMessage.Text = "";
+            }
+
+
+        }
+
+        private void btnAppendLongMessage_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        // I cant hear you, can you hear me
+    }
+
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CisGaAssignment2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
